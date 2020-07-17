2846744c1a978fd821ae39c1e527fc13bdb49ab7
linda <lindatochukwu30@gmail.com>
2020-07-17 01:37:24 +0100

    modify files-1

--- form.php
+++ form.php
@@ -1,10 +1,14 @@
 
-    <?php
+<?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
+$data = json_decode(file_get_contents('php://input'), true);
+$email =  $data['email'];
 
-echo "Welcome $name<br/> Your email address is $email"
+// create database connection and insert data
+
+// Send an email to that email address
+
+echo "Thanks for signing up, check your email for updates"
 
 ?>
 
